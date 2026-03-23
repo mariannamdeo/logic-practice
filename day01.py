8fb9182d8989beac14d03e253bb1e1d84ba70349
Marianna Martins de Oliveira <mariannamartinsdeoliveira@192.168.0.170>
2026-03-23 20:41:49 -0300

    meu primeiro código usando tudo que precisa :)

---
+++ day01.py
@@ -0,0 +1,27 @@
+#01
+print('hello, world again!')
+
+#02
+name = 'Marianna'
+age = '32'
+height = 1.61
+weight = 49
+dev = True
+
+print (f'Hello, {name}, your age is {age}, your height is {height}m, your weight is {weight}kg.')
+
+#03
+name_01 = input('Digite seu nome: ')
+age_01 = int(input('Qual é a sua idade? '))
+height_01 = float(input('Qual é a sua altura? '))
+weight_01 = float(input('Qual é o seu peso? '))
+
+print (f'Olá, {name_01}! Você tem {age_01}, com {height_01}m e {weight_01}kgs.' )
+
+#04
+number = int(input('Give me a number: '))
+
+new_number = number * 2
+
+print (f'2 * {number} is {new_number}.')
+
